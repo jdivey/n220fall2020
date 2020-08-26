12dc86e28edfb9697ca1b19bd54bc9bc12a7f976
jdivey <51710317+jdivey@users.noreply.github.com>
2020-08-26 15:37:47 -0400

    add classes

---
+++ labs/lab1/app.js
@@ -0,0 +1,19 @@
+class Rain {
+    constructor(height, weight) {
+        this.height=2,
+        this.width=2
+    }
+}
+
+class Raindrop {
+
+}
+
+class Ground {
+    constructor(height, width) {
+        this.height=10;
+        this.width=200;
+    }
+}
+
+var ground = new Ground(10, 200);
