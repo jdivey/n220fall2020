5ca518097dc1e17762791c060c21449a8736c30a
jdivey <51710317+jdivey@users.noreply.github.com>
2020-09-03 14:36:07 -0400

    Update app.js

--- labs/lab1/app.js
+++ labs/lab1/app.js
@@ -13,27 +13,47 @@ class Raindrop {
 
     update() {
         this.cy=this.cy + this.speed;
+
+        if(this.cy >500) {
+            this.cy = 0;
+        }
+
         fill(this.color);
         circle(this.cx, this.cy, this.radius);
     }
+
 }
 
 
 
 class Ground {
-    constructor(coordX, coordY, height, width, color) {
+    constructor(coordX, coordY, height, width) {
         this.coordX=coordX;
         this.coordY=coordY;
         this.height=height;
         this.width=width;
-        this.color= color;
+        this.r = 0;
+        this.g = 0;
+        this.b = 12.75;
+        this.color= [this.r,this.g,this.b];
     }
+
+    update() {
+        this.color= [this.r,this.g,this.b];
+        fill(this.color);
+        rect(this.coordX, this.coordY, this.width, this.height);
+        console.log(this.b);
+    }
+
+    
 }
 
 var rain = []
-var ground = new Ground(0, 475, 50, 600, [50, 74, 168]);
+var ground = new Ground(0, 475, 50, 600);
 rain[1] = new Raindrop(100, 0, 2, [50, 74, 168]);
 rain[2] = new Raindrop(200, 0, 2, [50, 74, 168]);
+rain[3] = new Raindrop(300, 0, 2, [50, 74, 168]);
+rain[4] = new Raindrop(400, 0, 2, [50, 74, 168]);
 
 
 
@@ -42,12 +62,48 @@ function setup() {
 }
 
 function draw() {
-    console.log("draw");
     background(163, 164, 168);
 
-    fill(ground.color);
-    rect(ground.coordX, ground.coordY, ground.width, ground.height);
+
+    ground.update();
     
-    rain[1].update();
-    rain[2].update();
+    rain.forEach(
+        function (raindrop) {
+            raindrop.update();
+            var isHitGround = isDropHitGround(raindrop.cx, raindrop.cy, ground.coordX, ground.coordY, ground.width, ground.height);
+                if(isHitGround) {
+                    ground.b += 1;
+
+        
+                }
+        }
+    )
+
+    
+
+    
+
+    
+}
+
+function isDropHitGround(pointX, pointY, boxX, boxY, boxWidth, boxHeight) {
+    //assume no hit
+    var hitDetected = false;
+
+    //check for hit
+    if((pointX > boxX) && (pointY > boxY)) {
+        if((pointX < boxX + boxWidth) && (pointY < boxY + boxHeight)) {
+            
+        //change to true if hitting
+        hitDetected = true;
+                
+            
+        }
+
+        
+    }
+    
+    return hitDetected;
+    
+    
 }
