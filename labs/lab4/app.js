11f291860442842a3cfb4498ac80ff92502fa745
jdivey <51710317+jdivey@users.noreply.github.com>
2020-09-24 22:09:12 -0400

    new

--- labs/lab4/app.js
+++ labs/lab4/app.js
@@ -3,9 +3,23 @@ class Grid {
 }
 
 class Xmark {
+    drawX() {
 
+    }
 }
 
 class Omark {
-    
+
+}
+
+let recOne = document.getElementById("recOne");
+recOne.addEventListener("click", drawX);
+function drawX(event) {
+    event.target.background(color, [a])
+}
+
+drawFrame();
+
+function drawFrame() {
+    requestAnimationFrame(drawFrame);
 }
