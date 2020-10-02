9e5c1c14da678c864b77829d8113c917d1f31c44
jdivey <51710317+jdivey@users.noreply.github.com>
2020-10-02 16:22:27 -0400

    morelab5

--- labs/lab5/app.js
+++ labs/lab5/app.js
@@ -1,3 +1,20 @@
-let application = document.getElementById("application");
 
+let application = document.getElementById("application");
 TweenMax.from(application, { duration: 2, x: -650, alpha: 0});
+
+
+let titlebox = document.getElementById("titlebox");
+let purp1 = document.getElementById("purp1");
+let purp2 = document.getElementById("purp2");
+let purp3 = document.getElementById("purp3");
+let purp4 = document.getElementById("purp4");
+let purp5 = document.getElementById("purp5");
+let purp6 = document.getElementById("purp6");
+
+function disapp(item) {
+    TweenMax.to(item, {duration: 0.7, opacity: 0});
+    
+}
+
+
+
